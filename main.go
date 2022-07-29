0c2b08018cd062c7cfd130a9c445fe66de59acf7
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-07-29 15:01:47 +0100

    adding the history command to check all previous commands

--- main.go
+++ main.go
@@ -1,46 +1,30 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 )
 
-type commandName string
-
-const (
-	// change Directory
-	ChangeDirectory commandName = "cd"
-	CreateDirectory commandName = "cr"
-	CommandsHistory commandName = "history"
-	CreateFile      commandName = "cf"
-	ReadFile        commandName = "rf"
-	Exit            commandName = "close"
-)
-
-type Command struct {
-	Name commandName
-	Args []string
-}
-
-func execCommand(input string) error {
-	input = strings.TrimSuffix(input, "\n")
-	cmd := exec.Command(input)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
-}
-
 func main() {
-	fmt.Println("Welcome to the simple shell!")
-	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Welcome to the simple Shell!")
+	var command string
+	var commandsHistory []string
 	for {
-		fmt.Print("> ")
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+		fmt.Print(">")
+		fmt.Scan(&command)
+		commandsHistory = append(commandsHistory, command)
+		args := strings.Split(command, " ")
+		switch args[0] {
+		case "cd":
+			os.Chdir(args[1])
+		case "history":
+			for i := 0; i < len(commandsHistory); i++ {
+				fmt.Println(commandsHistory[i])
+			}
+
+		default:
+			fmt.Println("unknown command")
 		}
 	}
 }
